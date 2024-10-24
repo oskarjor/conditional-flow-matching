a7fc4438b416f3d26db92857ccf44356ee4ee548
Oskar Jorgensen <oskarjorgensen@gmail.com>
2024-10-24 10:53:47 +0200

    update train_cifar script

--- scripts/train_cifar.sh
+++ scripts/train_cifar.sh
@@ -6,7 +6,12 @@
 #SBATCH --nodes=1               # Number of nodes
 #SBATCH --ntasks-per-node=1              # Number of tasks
 #SBATCH --time=00-00:10:00    # Upper time limit for the job (DD-HH:MM:SS)
+#SBATCH --output=train_cifar10.out
+#SBATCH --mail-user=oskarjor@ntnu.no
+#SBATCH --mail-type=ALL
 
 module load Python/3.10.15-GCCcore-14.2.0
+source /cluster/home/oskarjor/torchcfm/bin/activate
+pip install -r requirements.txt
 
 python examples/images/cifar10/train_cifar10.py
